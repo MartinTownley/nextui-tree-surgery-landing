0ffa0cd709c38d13639494d2e45c2b87ad81ac11
Merv <martinrtownley@gmail.com>
2024-03-18 17:35:18 +0000

    removed html action and useFormState() hook functionality, replaced with useState and react-hook-form handleSubmit()

--- app/actions/actions.tsx
+++ app/actions/actions.tsx
@@ -3,9 +3,15 @@
 import nodemailer from "nodemailer";
 import { revalidatePath } from "next/cache";
 import { z as zod } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-const { SMTP_SENDER, SMTP_RECIPIENT, SMTP_PASSWORD } = process.env;
 import { formDataSchema } from "@/app/lib/schema";
+
+const { SMTP_SENDER, SMTP_RECIPIENT, SMTP_PASSWORD } = process.env;
+
+type FieldInputs = zod.infer<typeof formDataSchema>;
+
+export type FormState = {
+  message: string;
+};
 
 const transport = nodemailer.createTransport({
   service: "gmail",
@@ -14,6 +20,42 @@ const transport = nodemailer.createTransport({
     pass: SMTP_PASSWORD,
   },
 });
+
+export async function sendEmail(data: FieldInputs) {
+  console.log("onsubmitAction", data);
+
+  const result = formDataSchema.safeParse(data);
+
+  if (result.success) {
+    return { success: true, data: result.data };
+  }
+
+  if (result.error) {
+    return { success: false, error: result.error.format() };
+  }
+
+  //const formData = Object.fromEntries(data);
+  // const parsed = formDataSchema.safeParse(formData);
+
+  // console.log("parsed", parsed);
+
+  // if (!parsed.success) {
+  //   return {
+  //     message: "Invalid form data",
+  //   };
+  // }
+
+  // const mailOptions = {
+  //   from: SMTP_SENDER,
+  //   to: SMTP_RECIPIENT,
+  //   subject: `New Inquiry from ${parsed.data.firstName} (${parsed.data.email})`,
+  //   text: parsed.data.message,
+  // };
+
+  // const info = await transport.sendMail(mailOptions);
+
+  //return { message: "success" };
+}
 
 export async function submitInquiry(prevState: any, formData: FormData) {
   console.log("formData", formData);
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,10 +3,10 @@
 import React from "react";
 import { Button, Input, Textarea } from "@nextui-org/react";
 import { SubmitButton } from "@/components/SubmitButton";
-import { submitInquiry } from "@/app/actions/actions";
+import { sendEmail } from "@/app/actions/actions";
 // @ts-ignore
 import { useFormState } from "react-dom";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { MdMail } from "react-icons/md";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
@@ -18,80 +18,82 @@ const initialState = {
   message: null,
 };
 
-type FormFields = zod.infer<typeof formDataSchema>;
-
-//type FormSchema = zod.output<typeof formDataSchema>;
+type FieldInputs = zod.infer<typeof formDataSchema>;
 
 const ContactForm = () => {
-  const form = useForm<FormFields>();
+  // const [state, formAction] = useFormState(onSubmitAction, initialState);
+  const [data, setData] = useState<FieldInputs>();
 
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<FormFields>({
+  const form = useForm<FieldInputs>({
+    resolver: zodResolver(formDataSchema),
     defaultValues: {
       firstName: "DefaultName",
       email: "default@email.com",
       message: "This is a default message",
     },
-    resolver: zodResolver(formDataSchema),
+    mode: "onChange",
   });
 
-  //const { register, control, handleSubmit } = form;
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = form;
 
-  const myOnSubmit: SubmitHandler<FormFields> = (data: FormFields) => {
-    console.log("form submitted", data);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const processForm: SubmitHandler<FieldInputs> = async (data) => {
+    const result = await sendEmail(data);
+
+    if (!result) {
+      console.log("something went wrong");
+      return;
+    }
+
+    if (result.error) {
+      // set local error state
+      console.log(result.error);
+    }
+
+    reset();
+    setData(result.data);
   };
-
-  //const formRef = useRef<HTMLFormElement>(null);
-
-  //const [state, formAction] = useFormState(submitInquiry, initialState);
-  // prior to form submission, "state" will be the initial state. After form submission, "state" will be whatever you're returning from the server action.
-
-  // destructure from state:
-  // const { data, error } = state;
-
-  //const nameErrorMsg = error?.name?._errors;
-  //const emailErrorMsg = error?.email?._errors;
-  //const messageErrorMsg = error?.message?._errors;
 
   return (
     <section className="container mx-auto mt-8 flex gap-8">
       <form
         noValidate
-        onSubmit={handleSubmit(myOnSubmit)}
-        //ref={formRef}
+        ref={formRef}
         //action={formAction}
+        onSubmit={handleSubmit(processForm)}
         className="flex flex-col mx-auto max-w-xs"
       >
         <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mb-4">
           <Input
             {...register("firstName")}
-            // isRequired
+            isRequired
             isClearable
             type="text"
             label="First Name"
             id="firstName"
-            // defaultValue=
+            defaultValue="DefaultName"
             errorMessage={errors.firstName && errors.firstName.message}
           />
-          {/* {errors.firstName && (
-            <div className="text-red-500">{errors.firstName.message}</div>
-          )} */}
         </div>
         <div className="mb-4">
           <Input
             {...register("email")}
-            // isRequired
+            isRequired
             isClearable
             type="email"
             label="Email"
             id="email"
             placeholder="name@email.com"
-            //defaultValue="test@mail.com"
+            defaultValue="default@mail.com"
             errorMessage={errors.email && errors.email.message}
+            //errorMessage={errors.email?.message}
             // isInvalid={false}
             startContent={
               <MdMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
@@ -101,10 +103,10 @@ const ContactForm = () => {
         <div className="mb-4">
           <Textarea
             {...register("message")}
-            // isRequired
+            isRequired
             label="Your message"
             placeholder="Enter your message"
-            //defaultValue="This is a placeholder message. Not a real message."
+            defaultValue="This is a default message. Not a real message."
             className="max-w-xs"
             id="message"
             errorMessage={errors.message && errors.message.message}
@@ -116,9 +118,9 @@ const ContactForm = () => {
       </form>
 
       <DevTool control={control} />
-      {/* <div className="flex-1 rounded-lg bg-cyan-600 p-8 text-white mx-auto overflow-y-auto">
-        <pre>{JSON.stringify(state, null, 2)}</pre>
-      </div> */}
+      <div className="flex-1 rounded-lg bg-cyan-600 p-8 text-white mx-auto overflow-y-auto">
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
     </section>
   );
 };
