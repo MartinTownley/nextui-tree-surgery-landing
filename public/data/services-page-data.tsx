0cac70d48980005696670d4a19bfbdc714ba2274
Merv <martinrtownley@gmail.com>
2024-08-20 19:02:37 +0100

    remove aws packages

---
+++ public/data/services-page-data.tsx
@@ -0,0 +1,44 @@
+export const servicesPageData = [
+  {
+    title: "Pruning",
+    description: `
+        Trees may require pruning to reduce weight on damaged or structurally unsound limbs. To remove hazardous deadwood or to create light and space in a small urban garden.
+  
+        We might wish to keep a small ornamental tree shapely and attractive or to establish the future shape of a young tree.
+  
+        With all pruning works it is important to analyse each individual tree contextually taking into consideration the nature of the species, its biology, its aesthetics and its interaction with its surroundings.
+  
+        Methods of pruning may include:
+        - Crown reductions
+        - Pollarding
+        - Formative pruning of young trees
+        - Crown lifting
+      `,
+  },
+  {
+    title: "Tree Removal",
+    description: `
+        Sometimes a tree may need to be removed. It may be diseased or structurally unsound, it may pose a risk to people or property. We rely on experience and teamwork to safely and efficiently dismantle trees in all manner of challenging urban environments.
+      `,
+  },
+  {
+    title: "Stump Grinding",
+    description: `
+        In London space is precious. Stumps take up space in a garden, they may need to be removed when constructing fences, sheds or garden rooms. Various hardwood species will resprout from a stump if left to its own devices. Our narrow access stump grinder can be brought through houses, narrow doorways and down stairs.
+      `,
+  },
+  {
+    title: "Hedge Management",
+    description: `
+        Hedges require routine maintenance, they may need trimming at regular intervals throughout the year.
+  
+        Hedge laying, once only associated with agricultural field boundaries is increasingly being established incorporated into urban spaces to increase biodiversity, filter pollution, as a security measure or simply for its aesthetic value.
+      `,
+  },
+  {
+    title: "Waste Disposal",
+    description: `
+        Green waste disposal can be challenging in London. We process as much waste as possible into woodchip which we offer to allotments and community gardens. Logs are used as biofuel, given to millers or taken to greenwoodworking groups who might turn an unloved or dangerous tree into something beautiful!
+      `,
+  },
+];
