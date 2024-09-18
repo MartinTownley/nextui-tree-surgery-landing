99f9e031ab977c6acaebc8c4b07b4a232e8b882a
Merv <martinrtownley@gmail.com>
2024-09-18 11:51:41 +0100

    tidy

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -5,7 +5,7 @@ export default function ServicesLayout({
 }) {
   return (
     <section className="flex flex-col justify-center gap-4 py-8 md:py-6 lg:items-start">
-      <div className="max-w-xxl w-full mx-auto lg:mx-0">{children}</div>
+      <div className="max-w-xxl w-full mx-auto lg:mx-0">{children}</div>   
     </section>
   );
 }
