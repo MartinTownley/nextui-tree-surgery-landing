ab83d856ef39ac963d5c71f006578c2165ff5a34
Merv <martinrtownley@gmail.com>
2024-10-07 00:18:32 +0100

    await result of Gallery component inside GalleryPage function

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -8,11 +8,12 @@ type ImageProps = {
   height: number;
 };
 export default async function GalleryPage() {
+  const galleryComponent = await Gallery();
   return (
     <div>
       <h1 className={title()}>Gallery</h1>
 
-      <Gallery />
+      {galleryComponent}
     </div>
   );
 }
