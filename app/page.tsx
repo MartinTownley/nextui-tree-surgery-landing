29b5ba8da0dde3163542dedb720b86faa67cb9d4
Merv <martinrtownley@gmail.com>
2024-10-16 23:24:12 +0100

    begin to add parallax scroll (needs fixing)

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,76 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import ServiceSwiper from "@/components/Services/ServiceSwiper";
-import Image from "next/image";
+import ServiceSection from "@/components/Services/ServiceSection";
 import NewContactForm from "@/components/NewContactForm";
 import { Toaster } from "sonner";
-import ServiceSection from "@/components/Services/ServiceSection";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="relative">
       <Toaster richColors position="top-center" />
-      <section className="flex flex-col items-start justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-left">
-          <h1 className={title()}>Your Friendly Neighbourhood Tree Surgery</h1>
-          <br />
-          <h2 className={subtitle({ class: "mt-2" })}>
-            At Sparrowhawk trees, we believe in more than tree surgery –
-            we&apos;re your partners in garden management, cultivating spaces
-            where nature and beauty coexist.
-          </h2>
-        </div>
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            href={siteConfig.links.docs}
-            className={buttonStyles({
-              color: "secondary",
-              variant: "shadow",
-            })}
+      <div className="hero-section relative flex flex-col items-start justify-center gap-4 py-8 md:py-10 mb-10 px-6">
+        <div
+          style={{
+            position: "relative",
+            height: "60vh",
+            width: "100%",
+            clipPath: "inset(0 0 0 0)",
+          }}
+        >
+          <div
+            style={{
+              position: "fixed",
+              height: "100%",
+              width: "100%",
+              left: "0",
+              top: "0",
+            }}
           >
-            View Services
-          </Link>
+            <Image
+              src="https://i.imgur.com/AwGWYah.jpg"
+              alt="Hero Image"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+              className="z-0 opacity-30"
+            />
+
+            <div className="relative z-10 inline-block max-w-lg text-left">
+              <h1 className={title()}>
+                Your Friendly Neighbourhood Tree Surgery
+              </h1>
+              <br />
+              <h2 className={subtitle({ class: "mt-2" })}>
+                At Sparrowhawk trees, we believe in more than tree surgery –
+                we&apos;re your partners in garden management, cultivating
+                spaces where nature and beauty coexist.
+              </h2>
+            </div>
+            <div className="flex gap-3 relative z-10">
+              <Link
+                isExternal
+                href={siteConfig.links.docs}
+                className={buttonStyles({
+                  color: "secondary",
+                  variant: "shadow",
+                })}
+              >
+                View Services
+              </Link>
+            </div>
+          </div>
         </div>
-      </section>
-      <section>
+      </div>
+
+      <div className="services-section mt-20 px-6">
         <ServiceSection />
-      </section>
-      <section id="contact" className="mt-10">
+      </div>
+      <div id="contact" className="contact-section mt-10">
         <NewContactForm />
-      </section>
+      </div>
     </section>
   );
 }
