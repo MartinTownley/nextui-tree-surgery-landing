1dacb5a46f6ba63b826049e9e229cf46ae600bdd
Merv <martinrtownley@gmail.com>
2024-10-17 15:00:30 +0100

    refactor and add opacity

--- app/page.tsx
+++ app/page.tsx
@@ -8,66 +8,59 @@ import { Toaster } from "sonner";
 import Image from "next/image";
 import { Button } from "@nextui-org/button";
 
+const bgImg = "https://i.imgur.com/AwGWYah.jpg";
+
 export default function Home() {
   return (
-    <section className="relative">
+    <section className="relative min-h-screen">
       <Toaster richColors position="top-center" />
-      <div className="hero-section relative flex flex-col items-start justify-center">
-        <div
-          className="py-8 md:py-10 px-6 "
-          style={{
-            position: "relative",
-            height: "70vh",
-            width: "100%",
-            clipPath: "inset(0 0 0 0)",
-          }}
-        >
-          <div
-            style={{
-              position: "fixed",
-              height: "100%",
-              width: "100%",
-              left: "0",
-              top: "0",
-            }}
-          >
-            <Image
-              src="https://i.imgur.com/AwGWYah.jpg"
-              alt="Hero Image"
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              className="z-0 opacity-30"
-            />
-          </div>
-          <div className="relative z-10 inline-block max-w-lg text-left">
-            <h1 className={title()}>
-              Your Friendly Neighbourhood Tree Surgery
-            </h1>
-            <br />
-            <h2 className={subtitle({ class: "mt-2" })}>
-              At Sparrowhawk trees, we believe in more than tree surgery –
-              we&apos;re your partners in garden management, cultivating spaces
-              where nature and beauty coexist.
-            </h2>
-          </div>
-          <div className="flex gap-3 relative z-10 py-6">
-            <Button
-              as={Link}
-              color="secondary"
-              radius="sm"
-              variant="shadow"
-              href="/#contact"
-            >
-              Get in Touch
-            </Button>
+      {/* Hero Section */}
+
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-fixed opacity-30"
+        style={{
+          backgroundImage: `url("https://i.imgur.com/AwGWYah.jpg")`,
+        }}
+      ></div>
+
+      <div className="relative z-10">
+        <div className="hero-section relative flex flex-col items-start justify-center">
+          {/* Hero Content Wrapper */}
+          <div className="py-8 md:py-10 px-6 relative h-[70vh] w-full">
+            {/* Hero Text */}
+            <div className="relative z-10 inline-block max-w-lg text-left">
+              <h1 className={title()}>
+                Your Friendly Neighbourhood Tree Surgery
+              </h1>
+              <br />
+              <h2 className={subtitle({ class: "mt-2" })}>
+                At Sparrowhawk trees, we believe in more than tree surgery –
+                we&apos;re your partners in garden management, cultivating
+                spaces where nature and beauty coexist.
+              </h2>
+            </div>
+            {/* Hero Button */}
+            <div className="flex gap-3 relative z-10 py-6">
+              <Button
+                as={Link}
+                color="secondary"
+                radius="sm"
+                variant="shadow"
+                href="/#contact"
+              >
+                Get in Touch
+              </Button>
+            </div>
           </div>
         </div>
       </div>
 
+      {/* Services Section */}
       <div className="services-section mt-20 px-6">
         <ServiceSection />
       </div>
+
+      {/* Contact Section */}
       <div id="contact" className="contact-section mt-10">
         <NewContactForm />
       </div>
