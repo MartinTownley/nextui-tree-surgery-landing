b59c6107eabbaeb25a358648aee017797e84abe5
Merv <martinrtownley@gmail.com>
2024-10-18 20:36:01 +0100

    add tailwind config changes re border

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+
+      3: "3px",
+      4: "4px",
+
+      6: "6px",
+
+      8: "8px",
+      10: "10px",
+      12: "12px",
+      14: "14px",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)"],
