2a2f6db46fb410d3d18afd9949b4bdc50be5ea30
Merv <martinrtownley@gmail.com>
2025-01-19 22:40:26 +0000

    edit footer

--- components/Footer2.tsx
+++ components/Footer2.tsx
@@ -1,11 +1,18 @@
 import { Link } from "@heroui/link"; // Assuming you're using NextUI for links
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa"; // For social icons
+import clsx from "clsx";
+import { roboto_mono } from "@/config/fonts";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center  text-center text-white">
-      <div className="w-full bg-black/20 p-4 text-center">
-        © 2024 Sparrowhawk Trees
+    <footer className="flex flex-col items-center  text-center text-gray-300">
+      <div
+        className={clsx(
+          "w-full bg-black/20 p-4 text-center",
+          roboto_mono.className
+        )}
+      >
+        [design & development by MT]
       </div>
     </footer>
   );
