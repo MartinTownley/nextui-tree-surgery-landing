4639e5c1f8171885269efd45cd7782a1d0610ae3
Martin Townley <martinrtownley@gmail.com>
2025-10-30 16:43:36 +0000

    about page layout improvements

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,11 @@ export default function AboutPage() {
   const imgUrl = "/about-imgs/profileImg.jpg";
   return (
     // Container
-    <div className="container flex flex-col-reverse lg:flex-row items-center mt-8 lg:mt-14">
+    <div className="container px-4 sm:px-6 lg:px-8 flex flex-col-reverse lg:flex-row items-center mt-8 lg:mt-14 lg:gap-12">
       {/* Content */}
       <div className="flex-1 flex-col items-center lg:items-start">
         <h1 className={`${title()} text-center`}>About us</h1>
-        <div className="text-lg mt-4 text-center lg:text-left mb-6">
+        <div className="text-lg mt-4 text-center lg:text-left mb-6 max-w-2xl mx-auto lg:mx-0">
           <p className="my-4">
             We are fully insured and qualified to undertake all aspects of tree
             surgery, from pruning and shaping to felling and stump removal. We
@@ -27,13 +27,14 @@ export default function AboutPage() {
           </p>
         </div>
       </div>
-      <div className="flex justify-center flex-1 m-10 md:m-12 lg:mb-0">
+      <div className="flex justify-center flex-1 my-8 lg:my-0 lg:ml-12">
         <Image
           src={imgUrl}
           alt="Profile Picture"
-          width={300}
-          height={375}
-          className="rounded-medium object-cover"
+          width={400}
+          height={500}
+          className="rounded-medium object-cover w-full max-w-[300px] md:max-w-[350px] lg:max-w-[400px] shadow-lg"
+          priority={true}
           // placeholder="blur"
           // style={{ objectFit: "" }}
         />
