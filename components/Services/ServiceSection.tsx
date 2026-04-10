0aa503f7b2b7216b48ae2b5325918235d09712a0
Martin Townley <martinrtownley@gmail.com>
2026-04-10 14:26:06 +0100

    small tidy-up change

--- components/Services/ServiceSection.tsx
+++ components/Services/ServiceSection.tsx
@@ -1,15 +1,10 @@
 // Services Section
-import {
-  fetchServicesWithImages,
-  Service,
-} from "@/app/lib/fetchServicesWithImages";
 import EmblaCarousel from "./EmblaCarousel";
 import type { EmblaOptionsType } from "embla-carousel";
 import servicesData from "@/public/data/services-data";
 
 const ServiceSection = async () => {
   const OPTIONS: EmblaOptionsType = { align: "start" };
-  // const servicesWithImages = await fetchServicesWithImages();
 
   return (
     <EmblaCarousel services={servicesData} options={OPTIONS}></EmblaCarousel>
